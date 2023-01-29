65876b36422264ff14b1d020a69f73298dac08cb
dell <navpreetsingh9878465121@gmail.com>
2023-01-29 16:45:25 +0530

    2 lecture

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val flstartButton  = binding.flStart
+
+        flstartButton.setOnClickListener{
+            Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_LONG).show()
+        }
     }
 }
